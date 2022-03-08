32573a59de73684213c22ca1f5550c4ea35c2e5f
HuailinTang <huailintang@yahoo.com>
2022-03-08 16:05:38 -0600

    question 5

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+mkdir ~/Desktop/linuxExercises/five
+
+x=1
+while [[ $x -le 5 ]];
+do  mkdir ~/Desktop/linuxExercises/five/dir$x;
+    cd ~/Desktop/linuxExercises/five/dir$x;
+    y=1;
+while [[ $y -le 4 ]];
+do echo > file$y.txt;
+   z=1
+   while [[ $z -le y ]];
+   do  echo "$z" >> file$y.txt;
+       z=$((z+1))
+      done;
+   y=$((y+1));
+done;
+x=$((x+1));
+done;
+   

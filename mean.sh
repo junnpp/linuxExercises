976d599c1a5268116159d8ed3242e2ace5b855ff
HuailinTang <huailintang@yahoo.com>
2022-03-09 23:21:41 -0600

    question 7

--- mean.sh
+++ mean.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+echo "usage: $0 <column> [file.csv]" 1>&2
+
+column=$1
+file=$2
+
+cut -d , -f $1 $2 | tail +2 | { sum=0; row=0; while read n; do  sum=$(($sum + $n)); row=$(($row + 1)); done; echo $((sum / row)); }
